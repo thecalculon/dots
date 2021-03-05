af55123675deb8d7caa67f59e6bbba9e556e4bfb
vikash <vikashp@tifrh>
2021-03-05 21:53:20 +0000

    initial config

---
+++ .init.sh
@@ -0,0 +1,8 @@
+# git configuration
+## See the video 
+##https://www.youtube.com/watch?v=tBoLDpTWVOM&ab_channel=DistroTube 
+## follow the line https://www.atlassian.com/git/tutorials/dotfiles
+
+# git init --bare $HOME/dotfiles
+# alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME' 
+# vikash@kemper ~> config config --local status.showUntrackedFiles no
