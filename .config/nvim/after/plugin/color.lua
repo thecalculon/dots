e2d491d75879bec7183de658ff2a051c8d2a94b3
vikash <vikashp@tifrh>
2024-02-24 19:11:22 +0100

    background set to terminal

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -68,4 +68,6 @@ require("modus-themes").setup({
 -- vim.cmd([[colorscheme modus]])
 vim.cmd("colorscheme modus")
 --
-vim.o.background = "dark" -- or "light" for light mode
+-- vim.o.background = "dark" -- or "light" for light mode
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
